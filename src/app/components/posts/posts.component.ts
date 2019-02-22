a6235e3782c1cf9913b7af1b2d57d9070bd27fe2
nfgoto <gotoflorian.pro@gmail.com>
2019-02-22 20:37:28 +0100

    fix delete

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -64,11 +64,15 @@ export class PostsComponent implements OnInit {
       // this.posts = this.posts.filter(p => p.id !== post.id);
       this.postService.deletePost(post).subscribe(
         delPost => {
-          this.posts.forEach((p, idx) => {
-            if (p.id === delPost.id) {
-              this.posts.splice(idx, 1);
-            }
-          });
+          this.posts = this.posts.filter(p => {
+            return p.id !== post.id;
+          }
+          )
+          // this.posts.forEach((p, idx) => {
+          //   if (p.id === delPost.id) {
+          //     this.posts.splice(idx, 1);
+          //   }
+          // });
         }
       );
     }
